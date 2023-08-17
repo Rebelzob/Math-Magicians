6235df2e7012d88cef6aa562f30aee231380e966
Rebelzob <karimdavid07@gmail.com>
2023-08-17 09:58:01 +0100

    Added Home contect such as title and paragrphs

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import '../assets/styles/home.css';
+/* eslint-disable max-len */
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to our page!</h2>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <p>
+        lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
